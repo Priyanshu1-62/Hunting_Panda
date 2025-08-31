020e128e38d8601f227a7ab4868fd4fad199b3c5
Priyanshu1-62 <bpriyanshu5678@gmail.com>
2025-09-01 02:48:44 +0530

    Add DSA section on home page

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,26 +1,31 @@
+import Image from 'next/image'
 import React from 'react'
 
 function page() {
   return (
     <>
-    <div className="h-60 bg-blue-200">
+    <div className="h-70">
 
     </div>
-    <div className="h-60 bg-blue-200">
+    <div className="flex bg-[#3c6e71]">
+        <div className="relative w-[40vw] h-[40vw]">
+            <Image src="/auth_pic2.png" fill/>
+        </div>
+    </div>
+    <div className="flex flex-row-reverse bg-[#d9d9d9]">
+        <div className="relative w-[40vw] h-[40vw]">
+            <Image src="/DSA_pic.png" fill/>
+        </div>
+    </div>
+    <div className="h-70 bg-[#3c6e71]">
 
     </div>
-    <div className="h-60 bg-blue-200">
+    <div className="h-70 bg-[#d9d9d9]">
 
     </div>
-    <div className="h-60 bg-blue-200">
+    <footer className="h-70 bg-neutral-700">
 
-    </div>
-    <div className="h-60 bg-blue-200">
-
-    </div>
-    <div className="h-60 bg-neutral-700">
-
-    </div>
+    </footer>
     </>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,8 +5,8 @@ import React from 'react'
 function Navbar() {
   return (
     <>
-    <div className="flex justify-center items-center mt-5 text-stone-800">
-        <ul className="flex gap-7 text-lg">
+    <div className="flex justify-center items-center text-stone-800">
+        <ul className="flex gap-7 my-5 text-lg">
             <li><Link href="/home">Home</Link></li>
             <li><Link href="about">About</Link></li>
             <li><Link href="blog">Blog</Link></li>
@@ -14,7 +14,7 @@ function Navbar() {
         </ul>
         <Search className="ml-6 hover:cursor-pointer"/>
     </div>
-    <div className="h-0.5 mt-3 mx-3 bg-stone-300"></div>
+    <div className="h-0.5 bg-[#d9d9d9]"></div>
     </>
   )
 }
