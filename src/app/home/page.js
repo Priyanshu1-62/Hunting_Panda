ecf649eaf318d5128d29e7409e4dc240c849c769
Priyanshu1-62 <bpriyanshu5678@gmail.com>
2025-09-01 20:50:09 +0530

    Add images to sections on home page

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -4,24 +4,28 @@ import React from 'react'
 function page() {
   return (
     <>
-    <div className="h-70">
-
+    <div className="flex h-70">
+      
     </div>
     <div className="flex bg-[#3c6e71]">
-        <div className="relative w-[40vw] h-[40vw]">
-            <Image src="/auth_pic2.png" fill/>
-        </div>
+      <div className="relative w-[40vw] h-[40vw]">
+          <Image src="/auth_pic2.png" fill/>
+      </div>
     </div>
     <div className="flex flex-row-reverse bg-[#d9d9d9]">
-        <div className="relative w-[40vw] h-[40vw]">
-            <Image src="/DSA_pic.png" fill/>
-        </div>
+      <div className="relative w-[40vw] h-[40vw]">
+          <Image src="/advJS_pic1.png" fill/>
+      </div>
     </div>
-    <div className="h-70 bg-[#3c6e71]">
-
+    <div className="flex bg-[#3c6e71]">
+      <div className="relative w-[40vw] h-[40vw]">
+          <Image src="/DSA_pic1.png" fill/>
+      </div>
     </div>
-    <div className="h-70 bg-[#d9d9d9]">
-
+    <div className="flex flex-row-reverse bg-[#d9d9d9]">
+      <div className="relative w-[40vw] h-[40vw]">
+          <Image src="/DSA_pic1.png" fill/>
+      </div>
     </div>
     <footer className="h-70 bg-neutral-700">
 
