56fa6ff195e9e96e2b79ccea63345e5807e483ce
Priyanshu1-62 <bpriyanshu5678@gmail.com>
2025-09-03 01:16:26 +0530

    Add explore button on home page

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,5 +1,6 @@
-import Image from 'next/image'
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 function page() {
   return (
@@ -19,13 +20,11 @@ function page() {
     </div>
     <div className="flex bg-[#3c6e71]">
       <div className="relative w-[40vw] h-[40vw]">
-          <Image src="/DSA_pic1.png" fill/>
+          <Image src="/tips_pic1.png" fill/>
       </div>
     </div>
-    <div className="flex flex-row-reverse bg-[#d9d9d9]">
-      <div className="relative w-[40vw] h-[40vw]">
-          <Image src="/DSA_pic1.png" fill/>
-      </div>
+    <div className="flex justify-center items-center h-100 bg-[#d9d9d9]">
+      <Link className="px-6 py-2 bg-[#22223b] text-white" href="blog">Explore more !</Link>
     </div>
     <footer className="h-70 bg-neutral-700">
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,8 +8,8 @@ function Navbar() {
     <div className="flex justify-center items-center text-stone-800">
         <ul className="flex gap-7 my-5 text-lg">
             <li><Link href="/home">Home</Link></li>
-            <li><Link href="about">About</Link></li>
             <li><Link href="blog">Blog</Link></li>
+            <li><Link href="about">About</Link></li>
             <li><Link href="contact">Contact</Link></li>
         </ul>
         <Search className="ml-6 hover:cursor-pointer"/>
