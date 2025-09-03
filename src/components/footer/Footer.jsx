3b9bcc9bb0657e07cb5e69c823f8907e374c22bc
Priyanshu1-62 <bpriyanshu5678@gmail.com>
2025-09-04 03:03:10 +0530

    Edit footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,8 @@ import { SiGithub, SiLinkedin, SiInstagram, SiFacebook, SiYoutube } from 'react-
 
 function Footer() {
   return (
-    <div className="flex justify-between h-80 px-24 bg-neutral-700 text-white">
+    <div className="bg-[#22223b] text-white">
+      <div className="flex justify-between h-80 px-24">
         <div className="flex justify-center items-center">
             <div className="relative h-35 w-35">
               <Image src="/panda_pic1.png" fill/>
@@ -14,6 +15,19 @@ function Footer() {
               <h2 className="text-4xl border-b-2 border-white">Hunting Panda</h2>
               <h2 className="text-xl">Hunt your doubts, catch clearity</h2>
             </div>
+        </div>
+        <div className="flex flex-col gap-1 justify-center">
+          <h4 className="font-bold mb-2">Learn</h4>
+          <h4>Lessons</h4>
+          <h4>Tips & Tricks</h4>
+          <h4>Code Snippets</h4>
+          <h4>Notes</h4>
+        </div>
+        <div className="flex flex-col gap-1 justify-center">
+          <h4 className="font-bold mb-2">Legal</h4>
+          <h4>Terms of Service</h4>
+          <h4>Privacy Policy</h4>
+          <h4>Disclaimer</h4>
         </div>
         <div className="flex flex-col justify-center items-center">
             <h2 className="">Follow us on social media</h2>
@@ -35,6 +49,10 @@ function Footer() {
               </Link>
             </div>
         </div>
+      </div>
+      <div className="flex justify-center items-center pt-2 pb-4 text-sm border-t-1 border-gray-600">
+        <h4>{`@ ${new Date().getFullYear()} Hunting Panda. All rights reserved.`}</h4>
+      </div>
     </div>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,10 +9,10 @@ function Navbar() {
         <ul className="flex gap-7 my-5 text-lg">
             <li><Link href="/home">Home</Link></li>
             <li><Link href="blog">Blog</Link></li>
+            <li><Link href="blog">Resources</Link></li>
             <li><Link href="about">About</Link></li>
             <li><Link href="contact">Contact</Link></li>
         </ul>
-        <Search className="ml-6 hover:cursor-pointer"/>
     </div>
     <div className="h-0.5 bg-[#d9d9d9]"></div>
     </>
