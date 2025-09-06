3f8252d6a526bd26ad06a634f652558f4dd5bbc9
Priyanshu1-62 <bpriyanshu5678@gmail.com>
2025-09-06 23:58:40 +0530

    Add resources/code-snippets route

---
+++ src/app/resources/code-snippets/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>Code snippets</div>
+  )
+}
+
+export default page
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
         <ul className="flex gap-7 my-5 text-lg font-[550] text-neutral-600">
             <li><Link className="hover:text-fuchsia-700 hover:text-shadow-lg" href="/home">Home</Link></li>
             <li><Link className="hover:text-fuchsia-700 hover:text-shadow-lg" href="blog">Blog</Link></li>
-            <li><Link className="hover:text-fuchsia-700 hover:text-shadow-lg" href="blog">Resources</Link></li>
+            <li><Link className="hover:text-fuchsia-700 hover:text-shadow-lg" href="resources">Resources</Link></li>
             <li><Link className="hover:text-fuchsia-700 hover:text-shadow-lg" href="about">About</Link></li>
             <li><Link className="hover:text-fuchsia-700 hover:text-shadow-lg" href="contact">Contact</Link></li>
         </ul>
