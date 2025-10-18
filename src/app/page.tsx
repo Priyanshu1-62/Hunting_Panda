982d77b6d16201f28562c28204612d6e62ed4417
Priyanshu1-62 <bpriyanshu5678@gmail.com>
2025-10-18 23:49:48 +0530

    Add Title in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,14 @@ function page() {
     <>
     <div className="relative h-[150vw] w-full">
       <Image src="/library1.jpg" fill alt="BG"/>
+      <div className="absolute top-44 w-full px-6">
+        <div className="flex justify-center">
+          <div className="flex flex-col lg:flex-row items-center gap-3 text-5xl font-bold text-amber-50">
+            <p>Hunt your doubts,</p>
+            <p>Catch Clearity</p>
+          </div>
+        </div>
+      </div>
     </div>
     </>
   )
